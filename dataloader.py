880bf85fec038d1b91994795db26e312bec21953
Jeanne Choo <jchoo@aisingapore.org>
2019-01-22 15:49:49 +0800

    added main to dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -9,10 +9,16 @@ from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader 
 
 def dataloader(batch_size):
-    '''downloads MNIST dataset, transforms data and returns train and val loaders for running model''' 
+    '''downloads MNIST dataset into folders, transforms data and returns train and val loaders for running model''' 
     data_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
     tr_set = MNIST(download=True, root=".", transform=data_transform, train=True)
     val_set = MNIST(download=False, root=".", transform=data_transform, train=False)
     train_loader = DataLoader(tr_set,batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_set,batch_size=batch_size, shuffle=False)
     return train_loader, val_loader
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('--batch_size', default=64, type=int, help="set batch size")
+    args = parser.parse_args()
+    dataloader(args.batch_size)
