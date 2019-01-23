bae610a86d8857ebc55883c43d20d72f936f4e86
jeannefukumaru <fukumaruuu@gmail.com>
2019-01-23 13:45:14 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,123 @@
+import json
+import logging
+import os
+import shutil
+from argparse import ArgumentParser
+
+import torch
+
+class RunningAverage():
+    """A simple class that maintains the running average of a quantity
+    
+    Example:
+    ```
+    loss_avg = RunningAverage()
+    loss_avg.update(2)
+    loss_avg.update(4)
+    loss_avg() = 3
+    ```
+    """
+    def __init__(self):
+        self.steps = 0
+        self.total = 0
+    
+    def update(self, val):
+        self.total += val
+        self.steps += 1
+    
+    def __call__(self):
+        return self.total/float(self.steps)
+
+def set_logger(log_path):
+    """Set the logger to log info in terminal and file `log_path`.
+    In general, it is useful to have a logger so that every output to the terminal is saved
+    in a permanent file. Here we save it to `model_dir/train.log`.
+    Example:
+    ```
+    logging.info("Starting training...")
+    ```
+    Args:
+        log_path: (string) where to log
+    """
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        # Logging to a file
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
+        logger.addHandler(file_handler)
+
+        # Logging to console
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter('%(message)s'))
+        logger.addHandler(stream_handler)
+
+def save_dict_to_json(d, json_path):
+    """Saves dict of floats in json file
+    Args:
+        d: (dict) of float-castable values (np.float, int, float, etc.)
+        json_path: (string) path to json file
+    """
+    with open(json_path, 'w') as f:
+        # We need to convert the values to float for json (it doesn't accept np.array, np.float, )
+        d = {k: float(v) for k, v in d.items()}
+        json.dump(d, f, indent=4)
+
+
+def save_checkpoint(state, is_best, checkpoint):
+    """Saves model and training parameters at checkpoint + 'last.pth.tar'. If is_best==True, also saves
+    checkpoint + 'best.pth.tar'
+    Args:
+        state: (dict) contains model's state_dict, may contain other keys such as epoch, optimizer state_dict
+        is_best: (bool) True if it is the best model seen till now
+        checkpoint: (string) folder where parameters are to be saved
+    """
+    filepath = os.path.join(checkpoint, 'last.pth.tar')
+    if not os.path.exists(checkpoint):
+        print("Checkpoint Directory does not exist! Making directory {}".format(checkpoint))
+        os.mkdir(checkpoint)
+    else:
+        print("Checkpoint Directory exists! ")
+    torch.save(state, filepath)
+    if is_best:
+        shutil.copyfile(filepath, os.path.join(checkpoint, 'best.pth.tar'))
+
+
+def load_checkpoint(checkpoint, model, optimizer=None):
+    """Loads model parameters (state_dict) from file_path. If optimizer is provided, loads state_dict of
+    optimizer assuming it is present in checkpoint.
+    Args:
+        checkpoint: (string) filename which needs to be loaded
+        model: (torch.nn.Module) model for which the parameters are loaded
+        optimizer: (torch.optim) optional: resume optimizer from checkpoint
+    """
+    if not os.path.exists(checkpoint):
+        raise("File doesn't exist {}".format(checkpoint))
+    checkpoint = torch.load(checkpoint)
+    model.load_state_dict(checkpoint['state_dict'])
+
+    if optimizer:
+        optimizer.load_state_dict(checkpoint['optim_dict'])
+
+    return checkpoint
+
+def get_args_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--model_dir', default='./', help="directory to store model weights, logs and other artefacts")
+    parser.add_argument('--lr', default=0.01, help="model learning rate")
+    parser.add_argument('--batch_size', default=64, type=int, help="set batch size")
+    parser.add_argument('--restore_file', default=None,
+                    help="Optional, name of the file in --model_dir containing weights to reload before \
+                    training")  # 'best' or 'train'
+    parser.add_argument('--epochs', default=10, help="number of epochs to train for")
+    parser.add_argument('--dropout', default=0.8, help="set dropout rate")
+    parser.add_argument('--save_summary_steps', default=100, help="number of steps at which to save model checkpoint")
+    parser.add_argument('--num_workers', default=4, help="number of workers to process data")
+    return parser 
+
+def get_arguments():
+    parser = get_args_parser()
+    args = parser.parse_args()
+    arguments = args.__dict__
+    return arguments
